5117d9d45393ef7c8f928a35483cd89841e0b409
masamin1810 <satomasamin1810@icloud.com>
2025-04-07 17:38:51 +0900

    Add files via upload

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,13 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+    private String firstName = "花子";
+    private String hobby = "趣味は読書です";
+
+    public void display() {
+        displayInfo(firstName, hobby);
+    }
+}
+
+
+//KatoHanako_Chapter18	趣味は読書です、と出力する
---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,14 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+    private String firstName = "一郎";
+    private String favoriteFood = "好きな食べ物はリンゴです";
+
+    public void display() {
+        displayInfo(firstName, favoriteFood);
+    }
+}
+
+
+
+//KatoIchiro_Chapter18	好きな食べ物はリンゴです、と出力する
