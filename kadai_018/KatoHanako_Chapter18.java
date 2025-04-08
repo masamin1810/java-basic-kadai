26812832c930c99aee3df4ca49d1cc0d8a8a25dc
masamin1810 <satomasamin1810@icloud.com>
2025-04-08 09:30:14 +0900

    Add files via upload

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,15 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+    public KatoHanako_Chapter18() {
+        this.givenName = "花子";
+    }
+
+    @Override
+    public void eachIntroduce() {
+        System.out.println("趣味は読書です");
+    }
+}
+
+
+//KatoHanako_Chapter18	趣味は読書です、と出力する
