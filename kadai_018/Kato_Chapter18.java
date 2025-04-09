f6ec5794c105c3612f5b17f915d88077ca81531e
masamin1810 <satomasamin1810@icloud.com>
2025-04-09 14:38:31 +0900

    Add files via upload

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,17 @@
+package kadai_018;
+
+public abstract class Kato_Chapter18 {
+    protected String familyName = "加藤";
+    protected String givenName;
+    protected String address = "東京都中野区〇〇";
+
+    // 抽象メソッド
+    public abstract void eachIntroduce();
+
+    // 共通のメソッド
+    public void execIntroduce() {
+        System.out.println("名前は " + familyName + givenName + " です");
+        System.out.println("住所は " + address + " です");
+    }
+    
+}
